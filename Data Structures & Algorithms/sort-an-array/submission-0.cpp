cfc68620b29f9a6be632f2d9b6e3053dc3136fb9
Devang Khandelwal <devang.122480@stu.upes.ac.in>
2026-08-08 14:51:45 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void merge(int low, int mid, int high ,  vector<int>&nums ){
+        vector<int> temp;
+        int i=low;
+        int j = mid+1;
+        while(i<=mid && j<= high){
+            if(nums[i]<= nums[j]){
+                temp.push_back(nums[i]);
+                i++;
+            }
+            else{
+                temp.push_back(nums[j]);
+                j++;
+            }
+        }
+        while(i<=mid){
+            temp.push_back(nums[i]);
+            i++;
+        }
+        while(j<=high){
+            temp.push_back(nums[j]);
+            j++;
+        }
+        for(int k=low; k<=high; k++){
+            nums[k]= temp[k-low];
+        }
+
+    }
+    void mergeSort(int low , int high, vector<int>& nums){
+        if(low>=high){
+            return;
+        }
+       int mid = low+(high-low)/2;
+        mergeSort(low,mid,nums);
+        mergeSort(mid+1,high, nums);
+        merge(low, mid, high , nums);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(0,nums.size()-1, nums);
+        return nums;
+    }
+};
