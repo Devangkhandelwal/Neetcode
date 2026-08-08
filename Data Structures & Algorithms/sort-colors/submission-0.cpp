9050fc3b67aaecd5cc58dc6835b303c43e829f26
Devang Khandelwal <devang.122480@stu.upes.ac.in>
2026-08-08 15:01:06 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& a) {
+        int low=0;
+        int mid=0; 
+        int high= a.size()-1;
+
+        while(mid<=high){
+            if(a[mid]== 0){
+                swap(a[low], a[mid]);
+                low++;
+                mid++;
+            }
+            else if(a[mid] == 1){
+                mid++;
+            }
+            else if(a[mid]== 2){
+                swap(a[mid],a[high]);
+                high--;
+            }
+        }
+    }
+};
