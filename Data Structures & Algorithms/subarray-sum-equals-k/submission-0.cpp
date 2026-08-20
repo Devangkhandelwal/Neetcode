fc466921150fe5b245fa9f62bc56dd98aaecea00
Devang Khandelwal <devang.122480@stu.upes.ac.in>
2026-08-20 22:02:00 +0530

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n= nums.size();
+        map<int,int> mp;
+        mp[0]=1;
+        int sum=0;
+        int ans=0;
+        for(int i=0; i<n;i++){
+            sum+=nums[i];
+            int remove=sum-k;
+            if(mp.find(remove)!= mp.end()){
+                ans+=mp[remove];
+            }
+            mp[sum]++;
+        }
+        return ans;
+        
+    }
+};
