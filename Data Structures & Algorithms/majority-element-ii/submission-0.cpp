f19e20d5b5611dafdfc0600ea4820fbfa7ebbd0b
Devang Khandelwal <devang.122480@stu.upes.ac.in>
2026-08-20 21:11:11 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n = nums.size();
+        vector<int> ans;
+        int mini = (int) n/3 + 1;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+
+            if(mp[nums[i]] == mini){
+                ans.push_back(nums[i]);
+            }
+            if(ans.size() == 2){
+                break;
+            }
+            
+        }
+        return ans;
+    }
+};
